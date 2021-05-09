94bbb40c9fb3c9c916e9d0fce5b32c7eb83ff48d
warhir <krzyzopolski.mariusz@gmail.com>
2021-05-09 17:12:41 +0200

    3a

---
+++ src/main/java/pl/pjatk/makrz/MakrzApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.makrz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakrzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakrzApplication.class, args);
+    }
+
+}
